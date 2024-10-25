1ada5272f2c9c16068068f03b5fc3275d6454132
Nhut Duy <ngophamnhutduy050302@gmail.com>
2024-10-25 08:53:39 +0700

    Cập nhật các tệp đã thay đổi

--- src/pages/users/theme/Header/index.js
+++ src/pages/users/theme/Header/index.js
@@ -1,11 +1,37 @@
 import { memo } from "react";
+import "./style.scss";
+import { BsFacebook, BsFillEmojiLaughingFill, BsFillThreadsFill, BsInstagram } from "react-icons/bs";
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <>
-     
-      <h1>Body</h1>
-     
-    </>
+    <div className="header_top">
+      <div className="container">
+        <div className="row">
+          <div className="col-6 header_top_left"> trai</div>
+          <div className="col-6 header_top_right">
+            <ul>
+              <li>
+                <Link to={""}>
+                  <BsFacebook />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <BsInstagram />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <BsFillThreadsFill />
+                </Link>
+
+                <span><BsFillEmojiLaughingFill /></span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 export default memo(Header);
