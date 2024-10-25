f81843b7b11fb782e39d55e57ab22063f1eecdb4
Nhut Duy <ngophamnhutduy050302@gmail.com>
2024-10-25 09:41:59 +0700

    update

--- src/pages/users/theme/Header/index.js
+++ src/pages/users/theme/Header/index.js
@@ -1,13 +1,20 @@
 import { memo } from "react";
 import "./style.scss";
-import { BsFacebook, BsFillEmojiLaughingFill, BsFillThreadsFill, BsInstagram } from "react-icons/bs";
+import { BsChatRightHeartFill, BsFacebook,BsFillThreadsFill, BsInstagram } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { format } from "../../../../utils/fomat";
 const Header = () => {
   return (
     <div className="header_top">
       <div className="container">
         <div className="row">
-          <div className="col-6 header_top_left"> trai</div>
+          <div className="col-6 header_top_left"> 
+            <ul>
+              <li><BsChatRightHeartFill />Hello@gmail.com</li>
+              <li>Miễn phí ship đơn từ {format(100000)}</li>
+            </ul>
+            
+          </div>
           <div className="col-6 header_top_right">
             <ul>
               <li>
@@ -24,9 +31,13 @@ const Header = () => {
                 <Link to={""}>
                   <BsFillThreadsFill />
                 </Link>
-
-                <span><BsFillEmojiLaughingFill /></span>
+                </li>
+                <li>
+                <Link to={""}>
+                  <BsFillThreadsFill />
+                </Link>
               </li>
+              <span className="login"> Đăng nhập</span>
             </ul>
           </div>
         </div>
